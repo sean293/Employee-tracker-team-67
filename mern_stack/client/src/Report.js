a672806efacad06dae4f497f75d0d3f25e7c16fb
sean293 <sean.hamilton@ufl.edu>
2024-04-23 14:03:29 -0400

    change colors of bars in bar chart

--- mern_stack/client/src/Report.js
+++ mern_stack/client/src/Report.js
@@ -58,9 +58,9 @@ const Report = () => {
 						datasets: [{
 							label: 'Total Time Clocked In',
 							data: values,
-							backgroundColor: 'rgba(75, 192, 192, 0.2)',
-							borderColor: 'rgba(75, 192, 192, 1)',
-							borderWidth: 1
+							backgroundColor: 'rgba(0, 84, 111, 0.65)',
+							borderColor: 'rgba(0, 84, 111, 1)',
+							borderWidth: 3
 						}]
 					},
 					options: {
