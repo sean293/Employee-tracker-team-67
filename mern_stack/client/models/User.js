1e9f93c3fac018fe730f14642e736af52078728b
erstwhilest <jbryan2@ufl.edu>
2024-04-24 23:00:49 -0400

    made default admin

--- mern_stack/client/models/User.js
+++ mern_stack/client/models/User.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema({
 	role: {
 		type: String,
 		enum: ['Employee', 'Manager', 'Administrator'],
-		default: 'Employee'
+		default: 'Administrator'
 	},
 });
 
